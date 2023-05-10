4f5c9941fcaad36e2ae3b542af8327863870f911
39bru <39bru@DESKTOP-3R312SJ>
2023-05-10 20:03:24 +0200

    DodaniPrviKontrolleri

---
+++ PaupProjekt/Controllers/GostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaupProjekt.Controllers
+{
+    public class GostController : Controller
+    {
+        // GET: Gost
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+    }
+}
