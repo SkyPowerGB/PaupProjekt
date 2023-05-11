f59325137ff227afdc58a327d61faa909de15d3e
39bru <39bru@DESKTOP-3R312SJ>
2023-05-11 12:24:18 +0200

    Dodana je baza

--- PaupProjekt/Controllers/GostController.cs
+++ PaupProjekt/Controllers/GostController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PaupProjekt.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,15 +8,20 @@ using System.Web.Mvc;
 namespace PaupProjekt.Controllers
 {
     public class GostController : Controller
-    {
+    { 
+        ServisVozilaDB db =new ServisVozilaDB();
         // GET: Gost
         public ActionResult Index()
         {
+            
             return View();
         }
 
         public ActionResult Kontakt()
         {
+            var baza = db.korisnici.ToList();
+            ViewBag.ime=
+            baza[0].korIme;
             return View();
         }
     }
---
+++ PaupProjekt/Models/ServisVozilaDB.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PaupProjekt.Models
+{
+
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+
+    public class ServisVozilaDB:DbContext 
+    {
+
+        public DbSet<korisnici> korisnici { get; set; }
+
+
+
+    }
+}
