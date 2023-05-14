d3a1e6d222c2c6efd5d0951599eee71a1cc91651
39bru <39bru@DESKTOP-3R312SJ>
2023-05-14 23:03:45 +0200

    Dodano Azuriraj ali ne radi

--- PaupProjekt/Controllers/RadnikController.cs
+++ PaupProjekt/Controllers/RadnikController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Ajax.Utilities;
+using Org.BouncyCastle.Crypto.Tls;
 using PaupProjekt.Models;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,8 @@ namespace PaupProjekt.Controllers
 {
     public class RadnikController : Controller
     {
-        ServisVozilaDB db= new ServisVozilaDB();
-        
+        ServisVozilaDB db = new ServisVozilaDB();
+
         // GET: Radnik
         public ActionResult Index()
         {
@@ -20,10 +21,68 @@ namespace PaupProjekt.Controllers
 
         public ActionResult klijenti() {
 
-         var   baza = db.servisTab.ToList();
+            var baza = db.servisTab.ToList();
 
             return View(baza);
         }
+
+        public ActionResult detalji(int? id) {
+
+            if (id == null) { return RedirectToAction("klijenti"); }
+
+
+            servis Servis = db.servisTab.FirstOrDefault(x => x.ServisID == id);
+
+            if (Servis == null) { return RedirectToAction("klijenti"); }
+
+
+
+
+                    return View(Servis);
+            }
+
+
+        public ActionResult Azuriraj(int? id)
+        {
+            servis nalozi = null;
+            if (!id.HasValue ) {
+                RedirectToAction("klijenti");
+
+            }
+          
+            nalozi= db.servisTab.FirstOrDefault(x=>x.ServisID==id);
+            if(nalozi == null) { RedirectToAction("klijenti"); }
+
+
+
+            return View(nalozi);
+        }
+
+
+            [HttpPost]
+        public ActionResult Azuriraj(servis s) {
+          
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(s).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                RedirectToAction("klijenti");
+
+            }
+
+            return View(s);
+            
+           
+
+        }
+
+
+
+
+
+
+
 
 
     }
--- PaupProjekt/Models/servis.cs
+++ PaupProjekt/Models/servis.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,19 +13,39 @@ namespace PaupProjekt.Models
     public class servis
     {
         [Key]
+
+        [Display(Name ="ID")]
         public int ServisID { get; set; }
 
+        [Display(Name = "VoziloID")]
+        [Required(ErrorMessage = "{0} je obavezan")]
         public int voziloID { get; set; }
 
+
+        [Display(Name = "VlasnikID")]
+        [Required(ErrorMessage = "{0} je obavezan")]
         public int VlasnikID { get; set; }
 
+        [Display(Name = "Datum")]
+        [Required(ErrorMessage = "{0} je obavezan")]
         public DateTime Datum { get; set; }
 
+        [Display(Name = "opisProblema")]
+        [Required(ErrorMessage = "{0} je obavezan")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage =
+        "{0} mora biti duljine minimalno {2} a maksimalno {1} znakova")]
         public string OpisProblema { get; set; }
 
+
+        [Display(Name = "StatusServisa")]
+        [Required(ErrorMessage = "{0} je obavezan")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage =
+        "{0} mora biti duljine minimalno {2} a maksimalno {1} znakova")]
         public string StatusServisa { get; set; }
 
+       public virtual vozilo VoziloVlasnika {  get; set; }
 
+       public virtual vlasnik VlasnikVozila { get; set; }
 
     }
 }
