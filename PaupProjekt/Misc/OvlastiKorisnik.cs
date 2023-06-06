57aa67e75957c75df747490d0ba57c0d51a87566
39bru <39bru@DESKTOP-3R312SJ>
2023-06-06 15:14:15 +0200

    dodani linkovi na navigaicjsku ovisno o korisniku

---
+++ PaupProjekt/Misc/OvlastiKorisnik.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaupProjekt.Misc
+{
+    public class OvlastiKorisnik
+    {
+
+        public const string Radnik = "MO";
+        public const string Admin = "AD";
+    }
+}
